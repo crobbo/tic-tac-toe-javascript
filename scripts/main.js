c090a9c9d5192371a1435e3570ba8457c7b44fc3
Christian Robinson <chrisdrobbo@gmail.com>
2021-11-29 10:35:49 +0000

    Added JavaScript logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,50 @@
 // Gameboard module
 
+var gameBoard = (function() {
+    'use strict';
+    var _board = ["X", "O", "X",
+                 "X", "O", "O",
+                 "O", "X", "X"];
+
+    function returnBoard() {
+        return _board;
+    }
+
+    return {
+        returnBoard
+    };
+})();
+
 // Display module
+var displayController = (function() {
+    'use strict';
 
-// Player Factory functon
+    function updateBoard(board) {
+        var childDivs = document.getElementById('board').getElementsByTagName('div');
+        for( let i=0; i < childDivs.length; i++ )
+            {
+            var childDiv = childDivs[i];
+            childDiv.innerText = board[i]
+            }
+    }
+    
+    return {
+        updateBoard
+    };
+})();
 
+
+// Player Factory functory
+const player = (function(counter) {
+    'use strict';
+    var _counter = counter;
+
+    return {
+        firstName, counter
+    };
+});
+
+// const Player1 = Player('X');
+// const Player2 = Player('O');
+
+displayController.updateBoard(gameBoard.returnBoard());
