7bd49b9cf2fa1d6ee6c80cefd359ec223d7f0c1f
Christian Robinson <chrisdrobbo@gmail.com>
2021-12-01 15:06:57 +0000

    Initial AI branch commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,41 @@
+// Initialize players
+const Player1 = Player('X', true);
+const Player2 = Player('O', false);
+
+// Event Listeners
+    document.body.addEventListener("click", (e) => {
+        if (e.target.classList.contains("js-place-counter")) {
+            let num = parseInt(e.path[0].classList[0].slice(-1));
+            let counter = '';
+            
+            if (gameBoard.isMultiPlayer()) {
+                gameBoard.startUpCheck();
+                gameBoard.setPlayerMove(num, counter);
+                gameBoard.multiPlayerLogic(num, counter);
+            } else if (gameBoard.isSinglePlayer()) {
+                gameBoard.SinglePlayerLogic();
+            }
+        }
+    });
+
+    document.body.addEventListener("click", (e) => {
+        if (e.target.classList.contains("js-restart")) {
+            gameBoard.restartGame();
+        }
+    });
+
+    // document.body.addEventListener("click", (e) => {
+    //     if (e.target.classList.contains("")) {
+            
+    //     }
+    // });
+
+    // document.body.addEventListener("click", (e) => {
+    //     if (e.target.classList.contains("")) {
+            
+    //     }
+    // });
+
 // Gameboard module  - controls the game logic.
 var gameBoard = (function() {
     'use strict';
@@ -7,6 +45,9 @@ var gameBoard = (function() {
                  "", "", "",
                  "", "", ""];
     var _count = 0;
+    var _singlePlayer = false;
+    var _mulitPlayer = false;
+
 
     // Private methods
     function _checkStraights() {
@@ -51,7 +92,7 @@ var gameBoard = (function() {
     }
 
     // Public methods
-    function gameLogic(num, counter) {
+    function multiPlayerLogic(num, counter) {
         if (_count === 9) { 
             _gameOver = true;
             return false
@@ -118,15 +159,25 @@ var gameBoard = (function() {
         } 
     }
 
+    function isSinglePlayer() {
+        return _isSinglePlayer;
+    }
+
+    function isMultiPlayer() { 
+        return _multiPlayer;
+    }
+
     return {
         returnBoard,
         updateBoard,
         checkForWinner,
         returnGameOver,
         returnCount,
-        gameLogic,
+        multiPlayerLogic,
         startUpCheck,
-        setPlayerMove
+        setPlayerMove,
+        isSinglePlayer,
+        isMultiPlayer
     };
 
 })();
